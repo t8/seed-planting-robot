f0cf641edfcf80628c8e73838ee3884eeff86ad0
tate <tateflyer1@gmail.com>
2020-01-08 10:57:02 -0500

    Added left-right functionality from Leah's function

--- servoMaster.py
+++ servoMaster.py
@@ -12,9 +12,9 @@ import math
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-pan = 27         # blue => PAN
-tilt = 17        # green => TILT
-rotate = 22      # yellow => ROTATE
+pan = 27  # blue => PAN
+tilt = 17  # green => TILT
+rotate = 22  # yellow => ROTATE
 
 GPIO.setup(tilt, GPIO.OUT)
 GPIO.setup(pan, GPIO.OUT)
@@ -32,27 +32,32 @@ def setServoAngle(servo, angle):
 
 
 def controlHorizonal(direction, length):
-    r=1
-    angle = (math.pi*r*length)/180
-    setServoAngle(pan, angle)
-    setServoAngle(tilt, -angle)
-    
-    
+    r = 1
+    angle = (math.pi * r * length) / 180
+    if direction == "left":
+        setServoAngle(pan, angle)
+        setServoAngle(tilt, -angle)
+    elif direction == "right":
+        setServoAngle(pan, -angle)
+        setServoAngle(tilt, angle)
 
-if __name__ == '__main__':
-    for i in range(30, 160, 15):
-        setServoAngle(pan, i)
-        setServoAngle(tilt, i)
-        setServoAngle(rotate, i)
-        print("MISSSYYYYY")
 
-    for i in range(150, 30, -15):
-        setServoAngle(pan, i)
-        setServoAngle(tilt, i)
-        setServoAngle(rotate, i)
-        print("LEAHHHHHH")
+# if __name__ == '__main__':
+#     for i in range(30, 160, 15):
+#         setServoAngle(pan, i)
+#         setServoAngle(tilt, i)
+#         setServoAngle(rotate, i)
+#         print("MISSSYYYYY")
+#
+#     for i in range(150, 30, -15):
+#         setServoAngle(pan, i)
+#         setServoAngle(tilt, i)
+#         setServoAngle(rotate, i)
+#         print("LEAHHHHHH")
+#
+#     setServoAngle(pan, 100)
+#     setServoAngle(tilt, 90)
+#     setServoAngle(rotate, 80)
 
-    setServoAngle(pan, 100)
-    setServoAngle(tilt, 90)
-    setServoAngle(rotate, 80)
-    GPIO.cleanup()
+controlHorizonal("left", 2)
+GPIO.cleanup()
