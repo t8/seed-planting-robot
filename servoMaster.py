0c56ebda4ec16145503c5539ee615d5a813945b4
tate <tateflyer1@gmail.com>
2020-01-08 09:23:16 -0500

    Updated servo code to work with multiple servos and a power supply

---
+++ servoMaster.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#
+#  Pan Tilt Servo Control
+#  Execute with parameter ==> sudo python3 servoCtrl.py <pan_angle> <tilt_angle>
+#
+#  MJRoBot.org 01Feb18
+
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+pan = 27         # blue => PAN
+tilt = 17        # green => TILT
+rotate = 22      # yellow => ROTATE
+
+GPIO.setup(tilt, GPIO.OUT)
+GPIO.setup(pan, GPIO.OUT)
+GPIO.setup(rotate, GPIO.OUT)
+
+
+def setServoAngle(servo, angle):
+    assert 30 <= angle <= 150
+    pwm = GPIO.PWM(servo, 50)
+    pwm.start(8)
+    dutyCycle = angle / 18. + 3.
+    pwm.ChangeDutyCycle(dutyCycle)
+    sleep(0.3)
+    pwm.stop()
+
+
+if __name__ == '__main__':
+    for i in range(30, 160, 15):
+        setServoAngle(pan, i)
+        setServoAngle(tilt, i)
+        setServoAngle(rotate, i)
+        print("MISSSYYYYY")
+
+    for i in range(150, 30, -15):
+        setServoAngle(pan, i)
+        setServoAngle(tilt, i)
+        setServoAngle(rotate, i)
+        print("LEAHHHHHH")
+
+    setServoAngle(pan, 100)
+    setServoAngle(tilt, 90)
+    setServoAngle(rotate, 80)
+    GPIO.cleanup()
