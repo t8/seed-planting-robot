ee54685e1eff1e203f74fac9e36b1566be73ef01
lgerm570 <56597095+lgerm570@users.noreply.github.com>
2020-01-14 11:01:08 -0500

    updated to control 2 step motors and servo

--- sketch_jan13a.ino
+++ sketch_jan13a.ino
@@ -4,9 +4,16 @@
 Servo name_servo;
 
 int servo_position = 0;
+AF_Stepper leftm(200, 2);
+AF_Stepper rightm(200, 1);
 
 void setup() {
   name_servo.attach(10);
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Stepper test!");
+ 
+  leftm.setSpeed(50);  // 10 rpm   
+  rightm.setSpeed(50);  // 10 rpm   
 }
 
 void loop() {
@@ -20,4 +27,22 @@ void loop() {
 //    delay(10);
 //  }
   name_servo.write(servo_position);
+  for(int s=0; s<200; s++)
+  {
+    leftm.step(1, FORWARD, DOUBLE);
+    rightm.step(1, FORWARD, DOUBLE); 
+//    delay(10);
+  }
+//  leftm.step(200, FORWARD, MICROSTEP); 
+//  rightm.step(200, FORWARD, MICROSTEP);
+//  motor.step(100, BACKWARD, SINGLE); 
+ 
+//  motor.step(100, FORWARD, DOUBLE); 
+//  motor.step(100, BACKWARD, DOUBLE);
+// 
+//  motor.step(100, FORWARD, INTERLEAVE); 
+//  motor.step(100, BACKWARD, INTERLEAVE); 
+// 
+//  motor.step(100, FORWARD, MICROSTEP); 
+//  motor.step(100, BACKWARD, MICROSTEP); 
 }
